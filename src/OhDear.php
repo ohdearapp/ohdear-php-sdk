561ad798d32c2fb02d8a977a9d84f8fd5bf025cb
Freek Van der Herten <freek@spatie.be>
2025-08-10 14:26:41 +0200

    wip

--- src/OhDear.php
+++ src/OhDear.php
@@ -35,6 +35,7 @@ class OhDear extends Connector implements HasPagination
 {
     use AcceptsJson;
     use AlwaysThrowOnErrors;
+
     use SupportsApplicationHealthChecksEndpoints;
     use SupportsBrokenLinksEndpoints;
     use SupportsCertificateHealthEndpoints;
